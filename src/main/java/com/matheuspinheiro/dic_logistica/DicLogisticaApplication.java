3eccf1d24a2eabd6d7edc7b780ec44223e6d48b8
Matheus Pinheiro <mathp237@gmail.com>
2024-07-05 18:57:20 -0300

    Create Spring Boot Project

---
+++ src/main/java/com/matheuspinheiro/dic_logistica/DicLogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.matheuspinheiro.dic_logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicLogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicLogisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuspinheiro/dic_logistica/DicLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuspinheiro.dic_logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
