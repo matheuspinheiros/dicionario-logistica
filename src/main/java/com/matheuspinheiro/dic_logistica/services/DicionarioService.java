526dcfab1c09cd74c6151ff67f2c83e55d7d7cc0
Matheus Pinheiro <mathp237@gmail.com>
2024-07-07 23:47:49 -0300

    Create UserService & DicionarioService

---
+++ src/main/java/com/matheuspinheiro/dic_logistica/services/DicionarioService.java
@@ -0,0 +1,60 @@
+package com.matheuspinheiro.dic_logistica.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheuspinheiro.dic_logistica.models.Palavra;
+import com.matheuspinheiro.dic_logistica.repositories.PalavraRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class DicionarioService {
+
+    @Autowired
+    private PalavraRepository palavraRepository;
+
+    public List<Palavra> findAll() {
+        return palavraRepository.findAll();
+    }
+
+    public Palavra findById(Long id) {
+        Optional<Palavra> palavra = this.palavraRepository.findById(id);
+        return palavra.orElseThrow(() -> new RuntimeException(
+                "palavra não encontrada! Id: " + id + ", Tipo: " + Palavra.class.getName()));
+    }
+
+    public Palavra findByPalavra(String palavra) {
+        Optional<Palavra> palavraResultado = this.palavraRepository.findByPalavra(palavra);
+        return palavraResultado.orElseThrow(() -> new RuntimeException(
+                "Palavra não encontrada! palavra: " + palavra + "tipo: " + Palavra.class.getName()));
+    }
+
+    @Transactional
+    public Palavra create(Palavra obj) {
+        obj.setId(null);
+        obj = this.palavraRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Palavra update(Palavra obj) {
+        Palavra newObj = findById(obj.getId());
+        newObj.setSignificado(obj.getSignificado());
+        newObj.setPalavra(newObj.getPalavra());
+        return this.palavraRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.palavraRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("não é possivel deletar pois há entidades relacionadas");
+        }
+    }
+
+}
