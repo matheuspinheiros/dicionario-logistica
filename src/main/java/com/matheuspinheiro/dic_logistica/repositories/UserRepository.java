0a20ff5190aa05cb170aca17fea91f2f5a927dc8
Matheus Pinheiro <mathp237@gmail.com>
2024-07-13 16:21:10 -0300

    Create UserDetailsServiceImpl and JWTUtil

--- src/main/java/com/matheuspinheiro/dic_logistica/repositories/UserRepository.java
+++ src/main/java/com/matheuspinheiro/dic_logistica/repositories/UserRepository.java
@@ -2,10 +2,14 @@ package com.matheuspinheiro.dic_logistica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matheuspinheiro.dic_logistica.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional
+    User findByUsername(String username);
 
 }
---
+++ src/main/java/com/matheuspinheiro/dic_logistica/security/JWTUtil.java
@@ -0,0 +1,64 @@
+package com.matheuspinheiro.dic_logistica.security;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JWTUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    public String generateToken(String username) {
+        SecretKey key = getKeyBySecret();
+        Instant now = Instant.now();
+        long expirationMillis = Long.parseLong(this.expiration);
+        Instant expirationTime = now.plus(expirationMillis, ChronoUnit.MILLIS);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(Date.from(expirationTime))
+                .signWith(key)
+                .compact();
+    }
+
+    private SecretKey getKeyBySecret() {
+        SecretKey key = Keys.hmacShaKeyFor(this.secret.getBytes());
+        return key;
+    }
+
+    public boolean isValidToken(String token) {
+        Claims claims = getClaims(token);
+        if (Objects.nonNull(claims)) {
+            String username = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+            if (Objects.nonNull(username) && Objects.nonNull(expirationDate) && now.before(expirationDate))
+                return true;
+        }
+        return false;
+    }
+
+    private Claims getClaims(String token) {
+        SecretKey key = getKeyBySecret();
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
