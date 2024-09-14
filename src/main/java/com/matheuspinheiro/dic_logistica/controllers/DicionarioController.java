277c4d8846c88b87d3b80489a3ae9a1f90a8394e
Matheus Pinheiro <mathp237@gmail.com>
2024-09-14 20:01:51 -0300

    Aplicando Swagger

--- src/main/java/com/matheuspinheiro/dic_logistica/controllers/DicionarioController.java
+++ src/main/java/com/matheuspinheiro/dic_logistica/controllers/DicionarioController.java
@@ -21,28 +21,49 @@ import com.matheuspinheiro.dic_logistica.models.Palavra.CreatePalavra;
 import com.matheuspinheiro.dic_logistica.models.Palavra.UpdatePalavra;
 import com.matheuspinheiro.dic_logistica.services.DicionarioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/dicionario")
 @Validated
+@Tag(name = "Dicionário TMS", description = "Operações com as palavras")
 public class DicionarioController {
 
     @Autowired
     private DicionarioService dicionarioService;
 
+    @Operation(summary = "Find All", description = "Busca uma lista com todas as palavras")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna a lista com o dicionário inteiro"),
+            @ApiResponse(responseCode = "403", description = "Usuário não autorizado, acesso negado") })
     @GetMapping("/palavras")
     public ResponseEntity<List<Palavra>> obterTodasAsPalavras() {
         List<Palavra> objs = this.dicionarioService.findAll();
         return ResponseEntity.ok().body(objs);
     }
 
+    @Operation(summary = "Find By Name", description = "Busca uma palavra pelo seu nome")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna a palavra, seu significado e seu id"),
+            @ApiResponse(responseCode = "403", description = "Usuário não autorizado, acesso negado"),
+            @ApiResponse(responseCode = "404", description = "Palavra não encontrada") })
     @GetMapping("/palavras/{palavra}")
     public ResponseEntity<Palavra> obterPalavraPeloNome(@PathVariable String palavra) {
         Palavra obj = this.dicionarioService.findByPalavra(palavra);
         return ResponseEntity.ok().body(obj);
     }
 
+    @Operation(summary = "Create Palavra", description = "Cria uma nova palavra no dicionário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Palavra criado com sucesso"),
+            @ApiResponse(responseCode = "403", description = "Usuário não autorizado, acesso negado"),
+            @ApiResponse(responseCode = "409", description = "Conflito ao salvar a palavra"),
+            @ApiResponse(responseCode = "422", description = "Não foi possível salvar a palavra"),
+            @ApiResponse(responseCode = "500", description = "Erro interno na aplicação, tente novamente mais tarde") })
     @PostMapping("/palavras")
     @Validated(CreatePalavra.class)
     public ResponseEntity<Void> create(@Valid @RequestBody Palavra obj) {
@@ -54,6 +75,13 @@ public class DicionarioController {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Change Significado", description = "Muda o significado de uma palavra, mudança feita pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Significado da palavra alterado com sucesso"),
+            @ApiResponse(responseCode = "403", description = "Usuário não autorizado, acesso negado"),
+            @ApiResponse(responseCode = "404", description = "Palavra não encontrada"),
+            @ApiResponse(responseCode = "409", description = "Conflito ao salvar a palavra"),
+            @ApiResponse(responseCode = "422", description = "Não foi possível salvar a palavra") })
     @PutMapping("/palavras/{id}")
     @Validated(UpdatePalavra.class)
     public ResponseEntity<Void> update(@Valid @RequestBody Palavra obj, @PathVariable Long id) {
@@ -62,6 +90,12 @@ public class DicionarioController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete Palavra", description = "Deleta uma palavra pelo seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Palavra deletada do banco de dados"),
+            @ApiResponse(responseCode = "403", description = "Usuário não autorizado, acesso negado"),
+            @ApiResponse(responseCode = "404", description = "Palavra não encontrada"),
+            @ApiResponse(responseCode = "409", description = "Não é possível deletar a palavra pois há entidades relacionadas") })
     @DeleteMapping("/palavras/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         this.dicionarioService.delete(id);
--- src/main/java/com/matheuspinheiro/dic_logistica/controllers/UserController.java
+++ src/main/java/com/matheuspinheiro/dic_logistica/controllers/UserController.java
@@ -20,30 +20,40 @@ import com.matheuspinheiro.dic_logistica.models.DTO.UserCreateDTO;
 import com.matheuspinheiro.dic_logistica.models.DTO.UserUpdateDTO;
 import com.matheuspinheiro.dic_logistica.services.UserService;
 
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user/")
 @Validated
+@OpenAPIDefinition(info = @Info(title = "API - Dicionário de Logística TMS", version = "1.0", description = "Uma API feita como dicionário para termos de logística", license = @License(name = "Apache 2.0")))
+@Tag(name = "User", description = "Operações com User")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    // @Operation(description = "Busca o usuário pelo id")
-    // @ApiResponses(value = {
-    // @ApiResponse(responseCode = "200", description = "Retorna o usuário"),
-    // @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
-    // })
+    @Operation(summary = "Get Users", description = "Busca o usuário pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o usuário"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "403", description = "Acesso Negado") })
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = this.userService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @Operation(summary = "Create Users", description = "Cria um usuário novo no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Conflito ao salvar as entidades") })
     @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody UserCreateDTO obj) {
         User user = this.userService.fromDTO(obj);
@@ -53,6 +63,10 @@ public class UserController {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Change Password", description = "Muda a senha do usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "A senha do usuário foi alterada com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Houve conflito ao tentar salvar a senha") })
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@Valid @RequestBody UserUpdateDTO obj, @PathVariable Long id) {
         obj.setId(id);
@@ -61,6 +75,11 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete User", description = "Deleta o usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "O usuário foi deletado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não existe ou não foi encontrado"),
+            @ApiResponse(responseCode = "409", description = "Não é possível deletar o usuário pois há entidades relacionadas") })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         this.userService.delete(id);
